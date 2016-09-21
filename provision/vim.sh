c2b5e6bb86358287eddc7e07557a74c8992483c3
Ignacio Carbajo <icarbajop@gmail.com>
2016-09-21 22:35:03 +0200

    Tidy up

--- provision/vim.sh
+++ provision/vim.sh
@@ -11,14 +11,16 @@ install_vim_package() {
 }
 
 mkdir -p ~/.vim/autoload/ ~/.vim/bundle
-if [ ! -f ~/.vim/autoload/pathogen.vim ]; then curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim > ~/.vim/autoload/pathogen.vim; fi
+if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
+  curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim \
+    > ~/.vim/autoload/pathogen.vim
+fi
 
 install_vim_package airblade/vim-gitgutter
-install_vim_package vim-airline/vim-airline
-install_vim_package vim-airline/vim-airline-themes
 install_vim_package ctrlpvim/ctrlp.vim
 install_vim_package elzr/vim-json
 install_vim_package jiangmiao/auto-pairs
+install_vim_package milkypostman/vim-togglelist
 install_vim_package nathanaelkane/vim-indent-guides
 install_vim_package ntpeters/vim-better-whitespace
 install_vim_package plasticboy/vim-markdown
@@ -26,8 +28,9 @@ install_vim_package scrooloose/nerdcommenter
 install_vim_package scrooloose/syntastic
 install_vim_package shougo/neocomplete.vim "sudo apt-get install -y vim-nox"
 install_vim_package shougo/vimproc.vim "cd ~/.vim/bundle/vimproc.vim && make; cd -"
+install_vim_package vim-airline/vim-airline
+install_vim_package vim-airline/vim-airline-themes
 install_vim_package vim-scripts/cream-showinvisibles
-install_vim_package milkypostman/vim-togglelist
 # haskell
   install_vim_package eagletmt/ghcmod-vim "stack install ghc-mod"
   install_vim_package €neovimhaskell/haskell-vim
