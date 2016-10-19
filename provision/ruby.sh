9e6093a71d49fddf9f65720d3ab9bb4fc8ece95d
Ignacio Carbajo <icarbajop@gmail.com>
2016-10-19 20:12:34 +0800

    Fix ruby provision

--- provision/ruby.sh
+++ provision/ruby.sh
@@ -2,9 +2,9 @@
 
 GEM_NAMES=(bundler)
 
-GEMS_LIST=$(gem list | grep "$GEM_NAME " | wc -l)
+GEMS_LIST=$(gem list)
 for GEM_NAME in "${GEM_NAMES[@]}"; do
-  if [ $(echo $GEMS_LIST | grep "$GEM_NAME " | wc -l) -eq "0" ]; then
+  if [ $(echo "$GEMS_LIST" | grep "$GEM_NAME " | wc -l) -eq "0" ]; then
     echo "doing: sudo gem install $GEM_NAME --no-ri --no-rdoc"
     sudo gem install "$GEM_NAME" --no-ri --no-rdoc
   fi
