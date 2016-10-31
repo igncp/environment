b231b1dae61f264124f9cebd03c29ccae54f20de
Ignacio Carbajo <icarbajop@gmail.com>
2016-10-31 22:01:39 +0800

    Add rvm provision

--- provision/ruby.sh
+++ provision/ruby.sh
@@ -1,13 +1,33 @@
 # ruby START
 
-GEM_NAMES=(bundler)
+RUBY_VERSION=2.2.4
+if [ ! -f ~/.ruby-installation-finished ]; then
+  sudo apt-get purge -y ruby ruby1.9
+  sudo apt-get autoremove -y
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+  curl -sSL https://get.rvm.io | bash -s stable
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+  ~/.rvm/bin/rvm install "$RUBY_VERSION"
+  ~/.rvm/bin/rvm use --default "$RUBY_VERSION"
+  touch ~/.ruby-installation-finished
+fi
 
-GEMS_LIST=$(gem list)
-for GEM_NAME in "${GEM_NAMES[@]}"; do
-  if [ $(echo "$GEMS_LIST" | grep "$GEM_NAME " | wc -l) -eq "0" ]; then
-    echo "doing: sudo gem install $GEM_NAME --no-ri --no-rdoc"
-    sudo gem install "$GEM_NAME" --no-ri --no-rdoc
-  fi
-done
+cat >> ~/.bashrc <<"EOF"
+
+export PATH="$PATH:~/.rvm/bin"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+EOF
+
+install_gems() {
+  GEMS_LIST=$(~/.rvm/gems/ruby-"$RUBY_VERSION"/wrappers/gem list)
+  for GEM_NAME in "$@"; do
+    if [ $(echo "$GEMS_LIST" | grep "^$GEM_NAME " | wc -l) -eq "0" ]; then
+      echo "doing: gem install $GEM_NAME --no-ri --no-rdoc"
+      ~/.rvm/gems/ruby-"$RUBY_VERSION"/wrappers/gem install "$GEM_NAME" --no-ri --no-rdoc
+    fi
+  done
+}
+
+install_gems bundler
 
 # ruby END
