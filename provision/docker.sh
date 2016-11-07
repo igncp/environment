b9256c879693c962f6fe2967a19cf3ca0cb97db4
Ignacio Carbajo <icarbajop@gmail.com>
2016-11-07 21:10:48 +0800

    Add docker provisioner

---
+++ provision/docker.sh
@@ -0,0 +1,19 @@
+# docker START
+
+if ! type docker > /dev/null 2>&1 ; then
+  sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+  echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+  sudo apt-get update
+  sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+  sudo apt-get install -y docker-engine
+  # use docker without sudo
+    sudo groupadd docker > /dev/null 2>&1
+    sudo gpasswd -a ${USER} docker
+    sudo service docker restart
+fi
+
+cat >> ~/.bash_aliases <<"EOF"
+alias DockerRmAll='docker stop $(docker ps -aq); docker rm $(docker ps -aq)'
+EOF
+
+# docker END
