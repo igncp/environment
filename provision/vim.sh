aff63f28b8d87b8e2005ed925273221e0d6c7af3
Ignacio Carbajo <icarbajop@gmail.com>
2016-12-28 21:35:13 +0800

    Fix deoplete with relative files

--- provision/vim.sh
+++ provision/vim.sh
@@ -207,6 +207,7 @@ cnoremap <C-K> <C-U>
 
 " deoplete
   let g:deoplete#enable_at_startup = 1
+  let g:deoplete#file#enable_buffer_path = 1
   inoremap <expr><tab> pumvisible() ? "\<c-n>" : "\<tab>"
   inoremap <expr><s-tab> pumvisible() ? "\<c-p>" : "\<tab>"
 
