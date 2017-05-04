f68cef453c95d9aba96a80f5e02edcbb07803cc6
Ignacio Carbajo <icarbajop@gmail.com>
2017-05-04 22:42:06 +0800

    Add jvm improvements

--- provision/jvm.sh
+++ provision/jvm.sh
@@ -2,6 +2,12 @@
 
 install_pacman_package jdk8-openjdk java
 
+cat >> ~/.vimrc <<"EOF"
+" https://github.com/Scuilion/gradle-syntastic-plugin.git
+let g:syntastic_java_checkers=['javac']
+let g:syntastic_java_javac_config_file_enabled = 1
+EOF
+
 # jvm END
 
 # jvm-extras START
