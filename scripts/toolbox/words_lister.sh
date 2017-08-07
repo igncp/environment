62b951369dabc959f46a3f44bf37de4032d84582
Ignacio Carbajo <icarbajop@gmail.com>
2017-08-07 21:49:08 +0800

    Improve toolbox scripts
    Add words lister
    Update yarn run

---
+++ scripts/toolbox/words_lister.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+DIR=$(find . -type d ! -path "*.git*" ! -path "*node_modules*" | \
+  fzf --height 100% --border -m  --ansi --header 'This can intensive for the memory')
+
+
+if [ -z "$DIR" ]; then
+  exit 0;
+fi
+
+cat > /tmp/lister-options <<"EOF"
+| grep -vP "^..?.?$"
+| grep "^[A-Z]"
+| grep "[A-Z]"
+| grep "[^0-9]"
+EOF
+
+OPTIONS=$(fzf -m --header 'Choose the options you want' < /tmp/lister-options)
+
+echo '
+__tmp_fn() {
+  DIR=$1;
+  echo "$(find "$DIR" -type f | xargs cat | sed "s|[,.():'"'"'=;{}, ]|\n|g" | grep "^[A-Za-z0-9]*$" )" '"$OPTIONS"'
+    | sort | uniq -c | sort -rn | less;
+} && __tmp_fn '"$DIR"
--- scripts/toolbox/yarn_run.sh
+++ scripts/toolbox/yarn_run.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
-NPM_RUN=$(npm run)
-echo "$NPM_RUN" > /tmp/npm-run
-COMMAND=$(echo "$NPM_RUN" | grep "^  [a-z]" |
+CACHED_FILE="/tmp/$(pwd | sed 's|^.||g; s|/|-|g')-yarn-run"
+
+if [ ! -f "$CACHED_FILE" ]; then
+  NPM_RUN=$(npm run)
+  echo "$NPM_RUN" > "$CACHED_FILE"
+fi
+
+COMMAND=$(grep "^  [a-z]" "$CACHED_FILE" |
   fzf --height 100% --border -m --ansi --multi --tac --preview-window right:70% \
   --preview "echo {} | xargs -Ill grep -A 1 '^  ll$' /tmp/npm-run")
+
+if [ -z "$COMMAND" ]; then
+  echo "# rm -rf $CACHED_FILE # to remove the cached file"
+  exit 0
+fi
 
 echo "yarn run $COMMAND"
