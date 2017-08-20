9cdcb7d5a1e1c5844507c8f09cadd2f7cbe57064
Ignacio Carbajo <icarbajop@gmail.com>
2017-08-20 23:10:16 +0800

    Fix yarn_run script

--- scripts/toolbox/yarn_run.sh
+++ scripts/toolbox/yarn_run.sh
@@ -9,7 +9,7 @@ fi
 
 COMMAND=$(grep "^  [a-z]" "$CACHED_FILE" |
   fzf --height 100% --border -m --ansi --multi --tac --preview-window right:70% \
-  --preview "echo {} | xargs -Ill grep -A 1 '^  ll$' /tmp/npm-run")
+  --preview "echo {} | xargs -Ill grep -A 1 '^  ll$' $CACHED_FILE")
 
 if [ -z "$COMMAND" ]; then
   echo "# rm -rf $CACHED_FILE # to remove the cached file"
