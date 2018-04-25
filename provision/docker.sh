56d76c6b2cf52575b0ade02b3487dc639bd94c3f
Ignacio Carbajo <icarbajop@gmail.com>
2018-04-25 23:19:20 +0800

    Add better comments

--- provision/docker.sh
+++ provision/docker.sh
@@ -14,8 +14,23 @@ echo 'export PATH=$PATH:/usr/local/lib/docker/bin' >> ~/.bashrc
 echo 'source_if_exists ~/.docker-completion.sh' >> ~/.bash_sources
 echo 'alias docker="sudo docker"' >> ~/.bash_aliases
 
-# Cleanup of subvolumes (run in `sudo su`)
-# Setup detailed here: https://gist.github.com/hopeseekr/cd2058e71d01deca5bae9f4e5a555440
-# for subvolume in /var/lib/docker/btrfs/subvolumes/*; do btrfs subvolume delete $subvolume; done
+# Setup with overlay2 filesystem
+  # BTRFS subvolumes are not well supported, and they come by default in Arch
+  # They generate stale data in /var/lib/docker/subvolumes
+  # They can easily fill the whole available space (sometimes, > 10 Gb in one day)
+  # It is better to configure a different filesystem. Follow the instructions:
+  # https://gist.github.com/hopeseekr/cd2058e71d01deca5bae9f4e5a555440
+  # __NOTE__: This will remove all existing Docker content
+  # The summary (in case the gist goes down):
+    # docker rm $(docker ps -aq); docker rmi -f $(docker images -q)
+    # # Stop dockerd
+    # sudo -s # all the next commands will be as root
+    # for subvolume in /var/lib/docker/btrfs/subvolumes/*; do btrfs subvolume delete $subvolume; done
+    # cd /media
+    # fallocate -l 2G docker-volume.img # 2Gb in this examples, in the gist is 10Gb
+    # mkfs.ext4 docker-volume.img
+    # mount -o loop -t ext4 /media/docker-volume.img /var/lib/docker
+    # echo "/media/docker-volume.img /var/lib/docker ext4 defaults 0 0" >> /etc/fstab
+  # At this point you can exit sudo and start dockerd. In the gist there are some checks commands to confirm
 
 # docker END
