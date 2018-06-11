142a05315dd8e19dd3303230d699709ddb5f9d30
Ignacio Carbajo <icarbajop@gmail.com>
2018-06-12 00:21:33 +0800

    Update installation scripts

--- arch-linux/installation/one.sh
+++ arch-linux/installation/one.sh
@@ -11,50 +11,17 @@
   # Menu > Devices > Shared Folders > Settings > Add new
   # Check: Make Permanent, Auto-Mount
   # Used name: project
-# @TODO: Improve partitions for performance
-# @TODO: Find a way to automate these too using curl
+# Run:
+  # @TODO: Improve partitions for performance
   # fdisk /dev/sda # create new partition: n p <enter> <enter> <enter> w
-  # mkfs.ext4 /dev/sda1
-  # mount /dev/sda1 /mnt
-  # pacstrap /mnt base
-  # genfstab -U /mnt >> /mnt/etc/fstab
-  # arch-chroot /mnt
-# @TODO: Automate these using curl
-# Run:
-  # pacman -Syy
-  # pacman -S git
-  # git clone https://github.com/igncp/vagrant-personal-cookbook.git /root/p
-  # sh /root/p/arch-linux/installation/one.sh
-
-echo "Set a new password for root:"
-passwd
-pacman -S grub --noconfirm
-grub-install --target=i386-pc /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
-useradd igncp -m
-echo "Set a new password for igncp:"
-passwd igncp
-pacman -S sudo --noconfirm
-echo "igncp ALL=(ALL) ALL" >> /etc/sudoers
-pacman -S openssh --noconfirm
-echo "alias ll='ls -lah'" >> /home/igncp/.bashrc
-echo "sudo dhcpcd" > /home/igncp/.init.sh
-echo "sudo systemctl start sshd.service" >> /home/igncp/.init.sh
-echo "sudo systemctl status sshd.service" >> /home/igncp/.init.sh
-# @TODO: Fix shared folder permissions
-echo "sudo mount -t vboxsf project /project" >> /home/igncp/.init.sh
-pacman -S --noconfirm virtualbox-guest-modules-arch virtualbox-guest-utils
-mkdir -p /project
-usermod -G vboxsf -a igncp
-chown igncp /project
-
-  # exit
-  # reboot
-# Boot existing OS
-# Remove ISO from devices
-  # Menu > Devices > Optical Drives > Uncheck Disc
-# Login as igncp
-# Run:
-  # sh .init.sh
-# From the host
-  # ssh -p 3022 igncp@127.0.0.1
+  # @TODO: Find how to copy this in VBox terminal. Find how to pipe to shell
+  # curl -L https://raw.githubusercontent.com/igncp/vagrant-personal-cookbook/master/arch-linux/installation/one.sh
+  mkfs.ext4 /dev/sda1
+  mount /dev/sda1 /mnt
+  pacstrap /mnt base
+  genfstab -U /mnt >> /mnt/etc/fstab
+  curl -o /mnt/root/start.sh L https://raw.githubusercontent.com/igncp/vagrant-personal-cookbook/master/arch-linux/installation/two.sh
+  echo ""
+  echo "Next steps:"
+  echo "arch-chroot /mnt"
+  echo "sh start.sh"
---
+++ arch-linux/installation/two.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+pacman -Syy
+pacman -S git
+echo "Set a new password for root:"
+passwd
+pacman -S grub --noconfirm
+grub-install --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+useradd igncp -m
+echo "Set a new password for igncp:"
+passwd igncp
+pacman -S sudo --noconfirm
+echo "igncp ALL=(ALL) ALL" >> /etc/sudoers
+pacman -S openssh --noconfirm
+echo "alias ll='ls -lah'" >> /home/igncp/.bashrc
+echo "sudo dhcpcd" > /home/igncp/.init.sh
+echo "sudo systemctl start sshd.service" >> /home/igncp/.init.sh
+echo "sudo systemctl status sshd.service" >> /home/igncp/.init.sh
+echo "sudo mount -t vboxsf project /project -o uid=1000,gid=1000" >> /home/igncp/.init.sh
+pacman -S --noconfirm virtualbox-guest-modules-arch virtualbox-guest-utils
+mkdir -p /project
+usermod -G vboxsf -a igncp
+chown igncp /project
+
+  # exit
+  # reboot
+# Boot existing OS
+# Remove ISO from devices
+  # Menu > Devices > Optical Drives > Uncheck Disc
+# Login as igncp
+# Run:
+  # sh .init.sh
+# From the host
+  # ssh -p 3022 igncp@127.0.0.1
