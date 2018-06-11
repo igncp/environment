8ff417eec46a08b96d7c3ff50f1d8168e97f8a59
Ignacio Carbajo <icarbajop@gmail.com>
2018-06-11 23:36:08 +0800

    Improve installation script

--- arch-linux/installation/one.sh
+++ arch-linux/installation/one.sh
@@ -7,6 +7,10 @@
 # Add port forwarding:
   # Menu > Device > Network > Network Settings > Advanced > Port Forwading
   # (name) ssh (type) TCP (host) 3022 (guest) 22
+# Add shared folder
+  # Menu > Devices > Shared Folders > Settings > Add new
+  # Check: Make Permanent, Auto-Mount
+  # Used name: project
 # @TODO: Improve partitions for performance
 # @TODO: Find a way to automate these too using curl
   # fdisk /dev/sda # create new partition: n p <enter> <enter> <enter> w
@@ -33,14 +37,23 @@ passwd igncp
 pacman -S sudo --noconfirm
 echo "igncp ALL=(ALL) ALL" >> /etc/sudoers
 pacman -S openssh --noconfirm
+echo "alias ll='ls -lah'" >> /home/igncp/.bashrc
 echo "sudo dhcpcd" > /home/igncp/.init.sh
 echo "sudo systemctl start sshd.service" >> /home/igncp/.init.sh
 echo "sudo systemctl status sshd.service" >> /home/igncp/.init.sh
+# @TODO: Fix shared folder permissions
+echo "sudo mount -t vboxsf project /project" >> /home/igncp/.init.sh
+pacman -S --noconfirm virtualbox-guest-modules-arch virtualbox-guest-utils
+mkdir -p /project
+usermod -G vboxsf -a igncp
+chown igncp /project
 
   # exit
   # reboot
 # Boot existing OS
 # Remove ISO from devices
+  # Menu > Devices > Optical Drives > Uncheck Disc
+# Login as igncp
 # Run:
   # sh .init.sh
 # From the host
