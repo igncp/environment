219787f2052e46781858d9d3055a8b360f27fe5a
Ignacio Carbajo <icarbajop@gmail.com>
2018-12-10 23:38:10 +0800

    Add vim map

--- arch-linux/provision/vim.sh
+++ arch-linux/provision/vim.sh
@@ -638,6 +638,11 @@ function! SetColors()
 endfunction
 
 call SetColors()
+
+" Know syntax type under cursor
+nnoremap <F10> :echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<'
+  \ . synIDattr(synID(line("."),col("."),0),"name") . "> lo<"
+  \ . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">"<CR>
 EOF
 
 cat >> ~/.bashrc <<"EOF"
