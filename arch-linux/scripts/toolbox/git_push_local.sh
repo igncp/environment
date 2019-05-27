0bc9b000b830a4b24d66b6ba2eb52be55340c1e9
Ignacio Carbajo <icarbajop@gmail.com>
2019-05-27 13:38:42 +0800

    Improve script

--- arch-linux/scripts/toolbox/git_push_local.sh
+++ arch-linux/scripts/toolbox/git_push_local.sh
@@ -8,8 +8,10 @@ else
   REMOTE=$(git remote | fzf)
 fi
 
+CURRENT_BRANCH=$(git branch | grep '\*' | cut -d ' ' -f2)
+
 BRANCH=$(git branch --color=always | grep -v '/HEAD\s' | sort |
-  fzf --height 100% --border -m --ansi --multi --tac --preview-window right:40% \
+  fzf --height 100% --border -m -q "$CURRENT_BRANCH" --ansi --multi --tac --preview-window right:40% \
   --preview 'git log --oneline --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) |
   head -'$LINES | sed 's/^..//' | cut -d' ' -f1 | sed 's#^remotes/##')
 
