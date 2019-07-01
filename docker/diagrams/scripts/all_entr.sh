478482326d4c776c2d827f0605bf103ddee2f53c
Ignacio Carbajo <icarbajop@gmail.com>
2019-07-01 16:08:41 +0800

    Add entr script for PlantUML

--- docker/diagrams/scripts/all_entr.sh
+++ docker/diagrams/scripts/all_entr.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-sh /home/ubuntu/scripts/dot_entr.sh &
-sh /home/ubuntu/scripts/mermaid_entr.sh &
+cd /home/ubuntu/scripts
+
+sh dot_entr.sh &
+sh mermaid_entr.sh &
+sh plantuml_entr.sh &
 
 # This will not end the script till all the rest have ended. If the script is
 # stopped, the rest of the parallel scripts will be stopped. More on `man bash`
