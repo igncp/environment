d84659c247c4e21e18deb271c22f9836d9b3fcc0
Ignacio Carbajo <icarbajop@gmail.com>
2019-09-10 23:32:40 +0800

    Add Dart provisions

--- arch-linux/provision/dart.sh
+++ arch-linux/provision/dart.sh
@@ -9,10 +9,16 @@ fi
 
 cat >> ~/.bashrc <<"EOF"
 export PATH="$PATH:/home/igncp/flutter/bin"
+export PATH="$PATH:/home/igncp/flutter/bin/cache/dart-sdk/bin"
+export PATH="$PATH:/home/igncp/.pub-cache/bin"
 EOF
 
 install_vim_package dart-lang/dart-vim-plugin
 
 install_pacman_package dart
+
+if ! type stagehand > /dev/null 2>&1 ; then
+  pub global activate stagehand
+fi
 
 # dart END
