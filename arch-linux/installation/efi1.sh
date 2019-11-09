a3087b98afd1c32285787152b8347c03ae996501
Ignacio Carbajo <icarbajop@gmail.com>
2019-11-09 11:32:43 +0800

    Add comments

--- arch-linux/installation/efi1.sh
+++ arch-linux/installation/efi1.sh
@@ -3,6 +3,10 @@
 # Installation scripts for UEFI Boot as host
 
 # Windows: Use UUI to setup a Live USB: https://www.pendrivelinux.com/universal-usb-installer-easy-as-1-2-3/
+# Arch: Use dd: https://wiki.archlinux.org/index.php/USB_flash_installation_media
+
+# First: Note down WIFI password
+
 # Whole process: https://www.youtube.com/watch?v=QP68hRqQTJ4
 # 1. `cfdisk /dev/sda`
 #   1. Choose to delete all
@@ -13,6 +17,7 @@
 # 1. `wifi-menu`
 # 1. Wait a few seconds
 # 1. `ping archlinux.org`
+# @TODO: Move this script to personal website
 # 1. `curl -L https://raw.githubusercontent.com/igncp/environment/master/arch-linux/installation/efi1.sh | bash`
 
 mkfs.fat -F32 /dev/sda1
