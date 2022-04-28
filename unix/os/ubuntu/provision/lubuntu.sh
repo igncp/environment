1c1ba9d7a970a928dd7debfc909cdf89c044fe5b
Ignacio Carbajo <icarbajop@gmail.com>
2022-04-28 23:41:44 +0200

    Move notes

--- unix/os/ubuntu/provision/lubuntu.sh
+++ unix/os/ubuntu/provision/lubuntu.sh
@@ -1,5 +1,9 @@
 # lubuntu START
 
+# ## Add a new icon in the launch bar
+# - Right click > Panel Settings
+# - Panel Applets > Application Launch and Task Bar > Preferences
+
 cat >> ~/.shell_aliases <<"EOF"
 LXPANEL_FILE="$HOME/.config/lxpanel/Lubuntu/panels/panel"
 alias LubuntuPanelSetAutohide='sed -i "s/  autohide=.$/  autohide=1/" "$LXPANEL_FILE" && lxpanelctl restart'
