57b92925971b2b16acf44b26dae0c583120098e7
Ignacio Carbajo <icarbajop@gmail.com>
2022-05-30 22:33:24 +0200

    Update script

--- unix/scripts/misc/take_screenshot.sh
+++ unix/scripts/misc/take_screenshot.sh
@@ -7,6 +7,6 @@ mkdir -p /home/igncp/Desktop
 TIME=$(date '+%Y-%m-%d_%H-%M-%S-%3N')
 TMP_FILE="/home/igncp/Desktop/screenshot.$TIME.png"
 
-import "$TMP_FILE"
+import -silent "$TMP_FILE"
 
 echo "$TMP_FILE created"
