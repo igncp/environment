95f24c1af06e378d5521892f25aea120353bcdf0
Ignacio Carbajo <icarbajop@gmail.com>
2022-08-02 23:55:00 +0200

    Start configuring unbound

--- unix/os/pi-hole.sh
+++ unix/os/pi-hole.sh
@@ -1,12 +1,9 @@
 # pi-hole START
 
-## pi-hole docker setup
-  # In the pi-hole running machine
-    # docker pull pihole/pihole
-    # sudo systemctl disable systemd-resolved.service
-  mkdir -p ~/.pi-hole
-  sudo touch ~/.pi-hole/hosts ; sudo chown root ~/.pi-hole/hosts
-  cat > ~/.pi-hole/docker-compose.yml <<"EOF"
+# https://docs.pi-hole.net/guides/dns/unbound/
+install_system_package unbound
+mkdir -p ~/.pi-hole
+cat > ~/.pi-hole/docker-compose.yml <<"EOF"
 version: "3"
 
 services:
@@ -24,7 +21,6 @@ services:
     volumes:
        - './etc-pihole/:/etc/pihole/'
        - './etc-dnsmasq.d/:/etc/dnsmasq.d/'
-       # - './hosts/:/etc/hosts' # Add this when copied from container
     dns:
       - 127.0.0.1
       - 1.1.1.1
@@ -38,19 +34,29 @@ alias PiHoleStop='(cd ~/.pi-hole/ && docker compose down)'
 alias PiHoleLogs='(cd ~/.pi-hole/ && docker compose logs -f pihole)'
 alias PiHolePassword='(cd ~/.pi-hole && docker compose exec pihole pihole -a -p)'
 alias PiHoleRepl='(cd ~/.pi-hole && docker compose exec pihole /bin/bash)'
+alias PiHoleUnboundUpdate=(sudo vim /etc/unbound/unbound.conf)
 PiHoleInit() {
   cd ~/.pi-hole
   if [ -z $(docker compose ps | ag running | ag pihole) ]; then echo 'You have to run docker'; return; fi
   docker compose exec -it pihole pihole logging off
   sudo sh -c "echo 'PRIVACYLEVEL=3' >> ./etc-pihole/pihole-FTL.conf"
   sudo sh -c "echo 'DBINTERVAL=60.0' >> ./etc-pihole/pihole-FTL.conf"
+  sudo systemctl disable --now systemd-resolved.service
+  sudo systemctl enable --now unbound
   docker compose down
   docker compose up -d
+}
+PiHoleRestardDNS() {
+  cd ~/.pi-hole/
+  sudo vim ./etc-pihole/custom.list && \
+    docker compose exec pihole pihole restartdns
 }
 EOF
 # Clients:
   # Arch Linux:
     # Update `netctl` profile config to include the IP address
     # DNS=('192.168.1.X')
+    # Update upstream DNS for pi-hole in: http://pi-hole-url/admin/settings.php?tab=dns
+        # Add logging: https://snippets.khromov.se/enable-logging-of-dns-queries-in-unbound-dns-resolver/
 
 # pi-hole END
